432175b8aa1c29734567e0a0a9d23eb267379efe
V4q3r0 <alejandrocarvajal2018@gmail.com>
2023-01-04 21:06:32 -0500

    Added services and logic for read and write file JSON

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,39 @@
 const { Router } = require('express')
 const router = Router();
+'use strict'
 
-router.get('/', (req, res) => {
-    res.json(
-        {
-            "Title": "Hello world!!"
-        }
-    )
+const fs = require('fs')
+
+router.get('/getVehicles', (req, res) => {
+    let data = fs.readFileSync('./src/assets/vehicles.json');
+
+    let vehicles = JSON.parse(data)
+
+    res.json(vehicles)
+})
+
+router.post('/registerVehicle', (req, res) => {
+
+    let data = fs.readFileSync('./src/assets/vehicles.json');
+    let vehicles = JSON.parse(data)
+    vehicles.push(req.body)
+    let request = JSON.stringify(vehicles)
+
+    fs.writeFileSync('./src/assets/vehicles.json', request)
+
+    res.json(vehicles)
+})
+
+router.put('/editVehicle/:vehicleId', (req, res) => {
+
+    let data = fs.readFileSync('./src/assets/vehicles.json');
+    let vehicles = JSON.parse(data)
+
+    vehicles = vehicles.filter(vehicle => vehicle.id != req.params.vehicleId);
+    vehicles.push(req.body)
+
+    res.json(vehicles)
+
 })
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const router = require('./routes/routes')
 
 app.set('port', process.env.PORT || 3000);
 app.set('json spaces', 2);
@@ -9,7 +10,7 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json())
 
-app.use(require('./routes/routes'))
+app.use('/api', router)
 
 app.listen(app.get('port'), () => {
     console.log(`Server listening on port ${app.get('port')}`);
